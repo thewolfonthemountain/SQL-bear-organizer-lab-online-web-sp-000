c4c3ac54c336ff1cc4e8e495c46a4a8e9a7ce9b0
Sean Langton <slangton1@gmail.com>
2018-12-31 03:42:05 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 2, 'male', 'black', 'happy', 'yes');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 2, 'male', 'black', 'happy', 'yes');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 3, 'male', 'black', 'happy', 'yes');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 3, 'male', 'black', 'happy', 'yes');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 3, 'male', 'black', 'happy', 'yes');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 4, 'male', 'black', 'happy', 'yes');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 5, 'male', 'black', 'happy', 'yes');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (Null, 6, 'male', 'black', 'happy', 'yes');
